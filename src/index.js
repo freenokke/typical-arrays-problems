078bbf926dbeeafbe90240b857f69645790e7268
freenokke <kosmatkov94@gmail.com>
2021-08-16 10:27:17 +0300

    feat: add task decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array == '') {
+    return 0
+  } 
+  return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array == '') {
+    return 0;
+  } 
+    return Math.max.apply(null, array)
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array == '') {
+    return 0
+  } 
+    let sum = 0;
+    for (let i = 0; i <= array.length - 1; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  
 }
